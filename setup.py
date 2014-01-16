87ac15fb98619b73768b983bb15b9b1720b9d194
Paul Jimenez <pj@place.org>
2014-01-16 09:37:33 -0500

    Bump to 1.4-dev

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup( author = 'Paul Jimenez'
      , py_modules = [ 'distribute_setup', 'cmdpy' ]
      , url = 'http://github.com/pjz/cmdpy'
      # there must be nothing on the following line after the = other than a string constant 
-     , version = '1.4'
+     , version = '1.4-dev'
      , zip_safe = False
       )
 
